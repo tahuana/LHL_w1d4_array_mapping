c7b36d9888ea38b7fef61c3e9ed3ba10db421753
tahuana <tahuana@gmail.com>
2017-06-01 15:53:38 +0000

    calculating Pythagorean Theorem

---
+++ array_mapping.js
@@ -0,0 +1,17 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(function (input){
+  x = Number(input.x);
+  y = Number(input.y);
+
+  return(Math.sqrt((x*x)+(y*y)));
+
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
+
